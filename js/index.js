d2d399713523314cab2a89d60a090624f89ef8d7
abdul-al-noman <nomanmd1st2001@gmail.com>
2022-09-03 16:08:56 +0600

    templact complect

--- js/index.js
+++ js/index.js
@@ -6,19 +6,25 @@ const newsFeedItem = ()=>{
 const allItmes = datas =>{
     const items =document.getElementById('news-item')
     datas.forEach(data => {
+        console.log(data)
         const div = document.createElement('div')
         div.classList.add('text-lg')
         div.classList.add('font-bold')
         div.classList.add('text-blue-700')
         div.classList.add('hover:text-blue-900')
+        div.classList.add('lg:mt-0')
+        div.classList.add('mt-3')
+        div.classList.add('ml-6')
+        div.classList.add('lg:ml-0')
         div.innerHTML=`
-            <h2 onclick="newsFeed(${data.category_id})">${data.category_name}</h2>
+            <h2 onclick="newsFeed('${data.category_id}')">${data.category_name}</h2>
         `
         items.appendChild(div)
     });
+    spiner(true)
 }
 const newsFeed =users=>{
-    fetch(` https://openapi.programming-hero.com/api/news/category/0${users}`)
+    fetch(` https://openapi.programming-hero.com/api/news/category/${users}`)
     .then(res=>res.json())
     .then(data=>newsFeedShow(data.data))
 }
@@ -32,16 +38,16 @@ const newsFeedShow =datas=>{
         console.log(data)
         const div =document.createElement('div')
         div.innerHTML=`
-        <div class="grid grid-cols-12 grid-rows-1 h-72 mb-6 border">
-            <div class="col-span-4 border m-3">
+        <div class="grid md:grid-cols-12 grid-cols-1 h-full mb-6 border md:p-0 sm:p-10 bg-slate-100">
+            <div class="col-span-4 m-3">
                 <img class="h-full" src="${data.image_url}" alt="">
             </div>   
-            <div class="col-span-8 grid grid-rows-3">
-                <div class="border row-span-2">
-                    <h3>${data.title}</h3>
-                    <p></p>
+            <div class="col-span-8 grid grid-rows-3 m-3">
+                <div class="row-span-2">
+                    <h3 class="text-2xl font-bold">${data.title}</h3>
+                    <p class="mt-3 text-ellipsis text-overflow-hidden" title="${data.details}">${data.details.slice(0,300)}...</p>
                 </div>
-                <div class="border row-span-1  text-center flex justify-between items-center">
+                <div class="row-span-1  text-center flex justify-between items-center">
                     <div class="flex justify-between">
                         <img class="w-10 mr-4 rounded-full " src="${data.author.img}" alt="">
                         <div class="text-left">
@@ -49,8 +55,8 @@ const newsFeedShow =datas=>{
                             <p>${data.author?data.author.published_date:"no data pound"}</p>
                         </div>
                     </div>
-                    <div class="col-span-3 ">${data.total_view?data.total_view:'no data pound '}</div>
-                    <div class="col-span-3">c</div>
+                    <div class="col-span-3 "> <i class="fa-solid fa-eye"></i> ${data.total_view?data.total_view:'no data pound '}</div>
+                    <div class="col-span-3"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i></div>
                     <div class="col-span-3 mr-4">
                         <label onclick="modal('${data._id}')" for="my-modal" class="btn modal-button">click</label>
                     </div>
@@ -60,6 +66,7 @@ const newsFeedShow =datas=>{
         `
         newsSection.appendChild(div)
     })
+    spiner(false)
 }
 const modal = users=>{
     fetch(` https://openapi.programming-hero.com/api/news/${users}`)
@@ -81,4 +88,13 @@ const modalData=datas=>{
     </div>
     `
 } 
+const spiner =check=>{
+    const spinerData =document.getElementById('spinerItem')
+    if(check){
+        spinerData.classList.remove('d-none')
+    }
+    else{
+        spinerData.classList.add('d-none')
+    }
+}
 newsFeedItem()
